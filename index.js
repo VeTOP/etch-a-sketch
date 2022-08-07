64123a3fd1221ca4aa9a8206e1826a757bba710d
Vera <moirabestwaifu@gmail.com>
2022-08-07 21:43:24 +0200

    Saving before attempting to create a 16x16 grid with spans and divs

---
+++ index.js
@@ -0,0 +1,18 @@
+// create a for loop that makes a 16by16 matrix
+
+/* 
+// original div in the html; class container
+let matrix1 = [];
+// matrix that wraps in the elements; class row
+let matrix2 = [];
+
+for (let i=0; i<16; i++){
+    // add items; span class item
+    matrix2.push(i);
+    // add div class row to div class container
+    matrix1.push(matrix2);
+}
+
+console.log(matrix1)
+*/
+
