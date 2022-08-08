c05018d431c2c30158c8c0dae5aa0fdf53761115
Vera <moirabestwaifu@gmail.com>
2022-08-08 14:48:55 +0200

    Created grid with items

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const container = document.querySelector('.container');
 
 for (let i=0; i<16; i++){
     let row = document.createElement('div');
+    row.classList.add('row');
     container.append(row)
     for (let j=0; j<16; j++){
-    let item = document.createElement('span');
-    row.append(item);
+        let item = document.createElement('div');
+        item.classList.add('item');
+        row.append(item);
     }
 }
