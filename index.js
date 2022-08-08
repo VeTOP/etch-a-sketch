5f7a46fa57542c7b48b5a90cddcca74ad2836a35
Vera <moirabestwaifu@gmail.com>
2022-08-08 12:26:07 +0200

    Created a for loop that makes a 16x16 matrix

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 // create a for loop that makes a 16by16 matrix
-
-/* 
-// original div in the html; class container
-let matrix1 = [];
-// matrix that wraps in the elements; class row
-let matrix2 = [];
+const container = document.querySelector('.container');
 
 for (let i=0; i<16; i++){
-    // add items; span class item
-    matrix2.push(i);
-    // add div class row to div class container
-    matrix1.push(matrix2);
+    let row = document.createElement('div');
+    container.append(row)
+    for (let j=0; j<16; j++){
+    let item = document.createElement('span');
+    row.append(item);
+    }
 }
-
-console.log(matrix1)
-*/
-
