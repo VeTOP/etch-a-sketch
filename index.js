39aec220b49e6a66ba553d97fc42dd5bf9bde4cf
Vera <moirabestwaifu@gmail.com>
2022-08-09 14:47:35 +0200

    added working resize button

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-let containerDensity = prompt("Tell me the density:");
-let density = 700 / +containerDensity;
-
-
 const container = document.querySelector('.container');
 
 
+let containerDensity;
+let density;
 
-for (let i=0; i<containerDensity; i++){
-    let row = document.createElement('div');
-    row.classList.add('row');
-    container.append(row)
-    for (let j=0; j<containerDensity; j++){
-        let item = document.createElement('div');
-        item.style.width = density+'px';
-        item.style.height = density+'px';
-        item.classList.add('item');
-        row.append(item);
+function draw(){
+    containerDensity = prompt("Tell me the density:");
+    density = 700 / +containerDensity;
+    for (let i=0; i<containerDensity; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.append(row)
+        for (let j=0; j<containerDensity; j++){
+            let item = document.createElement('div');
+            item.style.width = density+'px';
+            item.style.height = density+'px';
+            item.classList.add('item');
+            row.append(item);
+        }
     }
 }
+
+draw();
 
 document.querySelector('.container').addEventListener('mouseover', function(e){
     const target = e.target;
@@ -26,9 +30,25 @@ document.querySelector('.container').addEventListener('mouseover', function(e){
     }
 })
 
-const items = document.querySelectorAll('.item');
+
+const row = document.querySelector('.row');
+const item = document.querySelector('.item');
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function resize(){
+    removeAllChildNodes(container);
+    draw();
+}
+
+let items;
 function shake(){
+    items = document.querySelectorAll('.item');
     for (let z=0; z<(containerDensity*containerDensity); z++){
-        items[z].style.backgroundColor = 'antiquewhite'
+        items[z].style.backgroundColor = 'antiquewhite';
     }
 }
